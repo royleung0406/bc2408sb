fb620a8fb3ef8872996cb4fb58023bdcf2234759
roy. leung <royleung0406@gmail.com>
2024-10-02 17:49:09 +0800

    10022024

---
+++ bc2408-sb/demo-sb-helloworld/src/main/java/com/bootcamp/demo/demo_sb_helloworld/HelloworldController.java
@@ -0,0 +1,16 @@
+package com.bootcamp.demo.demo_sb_helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloworldController {
+  //endpoint to locate the web service
+  //http://127.0.0.1:8080/hello
+  @GetMapping(value = "/hello")
+  public String sayHello(){
+    return "Hello World";
+  }
+}
---
+++ bc2408-sb/demo-sb-helloworld/src/main/java/com/bootcamp/demo/demo_sb_helloworld/Person.java
@@ -0,0 +1,7 @@
+package com.bootcamp.demo.demo_sb_helloworld;
+import lombok.Getter;
+
+@Getter
+public class Person {
+ 
+}
